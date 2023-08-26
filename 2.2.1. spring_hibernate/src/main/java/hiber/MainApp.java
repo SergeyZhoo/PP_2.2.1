9e8502f41f9b120d22f71d560a86b5cfc9afa162
SergeyZhoo <sjoo@ya.ru>
2023-08-26 23:09:18 +0300

    v1.0

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,33 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      Car car1 = new Car();
+      car1.setModel("Model1");
+      car1.setSeries(1231);
+      user1.setCar(car1);
+      userService.add(user1);
+
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      Car car2 = new Car();
+      car2.setModel("Model2");
+      car2.setSeries(1232);
+      user2.setCar(car2);
+      userService.add(user2);
+
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      Car car3 = new Car();
+      car3.setModel("Model3");
+      car3.setSeries(1233);
+      user3.setCar(car3);
+      userService.add(user3);
+
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+      Car car4 = new Car();
+      car4.setModel("Model4");
+      car4.setSeries(1234);
+      user4.setCar(car4);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -28,6 +52,11 @@ public class MainApp {
          System.out.println("Email = "+user.getEmail());
          System.out.println();
       }
+
+      System.out.println(userService.getUserByCarModelAndSeries("Model1", 1231));
+      System.out.println(userService.getUserByCarModelAndSeries("Model2", 1232));
+      System.out.println(userService.getUserByCarModelAndSeries("Model3", 1233));
+      System.out.println(userService.getUserByCarModelAndSeries("Model4", 1234));
 
       context.close();
    }
--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -26,4 +26,12 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUserByCarModelAndSeries(String model, int series) {
+      TypedQuery<User> query = sessionFactory.getCurrentSession()
+              .createQuery("SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series", User.class);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.getSingleResult();
+   }
 }
